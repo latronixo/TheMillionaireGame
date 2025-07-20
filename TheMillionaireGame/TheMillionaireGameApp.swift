3e682889c43b54e86dd5ae44406ac94ac2552d17
Valentin <pamerra@mail.ru>
2025-07-20 18:38:21 +0300

    Initial Commit

---
+++ TheMillionaireGame/TheMillionaireGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMillionaireGameApp.swift
+//  TheMillionaireGame
+//
+//  Created by Валентин on 20.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct TheMillionaireGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
