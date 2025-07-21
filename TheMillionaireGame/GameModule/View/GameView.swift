248339fc71740299e063aa2061cf3cf3fe2d69cf
Valentin <pamerra@mail.ru>
2025-07-21 23:40:40 +0300

    Первый вопрос и ответы из API отображаюся на экране

--- TheMillionaireGame/GameModule/View/GameView.swift
+++ TheMillionaireGame/GameModule/View/GameView.swift
@@ -8,6 +8,12 @@
 import SwiftUI
 
 struct GameView: View {
+    @StateObject private var viewModel: GameViewModel
+    
+    init(viewModel: GameViewModel = GameViewModel()) {
+        _viewModel = StateObject(wrappedValue: viewModel)
+    }
+    
     var body: some View {
         ZStack{
             LinearGradient(colors: [Color(red: 55/255, green: 76/255, blue: 148/255), Color(red: 16/255, green: 14/255, blue: 22/255)], startPoint: .topLeading, endPoint: .bottomTrailing)
@@ -17,14 +23,75 @@ struct GameView: View {
                 HeaderView()
                 TimerView()
                 VStack(spacing: 60){
-                    QuestionAndAnswerView()
+                    if viewModel.isLoading {
+                        ProgressView("Loading questions...")
+                    } else if let error = viewModel.errorMessage {
+                        Text("Error: \(error)")
+                            .foregroundColor(.red)
+                            .multilineTextAlignment(.center)
+                            .padding()
+                    } else {
+                        Text(viewModel.currentTextQuestion)
+                            .foregroundStyle(.white)
+                            .font(.system(size: 24))
+                            .fontWeight(.bold)
+                            .lineLimit(4)
+
+                        VStack(alignment: .leading, spacing: 70) {
+                            ForEach(0...3, id: \.self) { index in
+                                Button {
+                                    viewModel.nextQuestion()
+                                } label: {
+                                    Text("\(viewModel.ABCD[index]) \(viewModel.answers[index])")
+                                        .font(.system(size: 20))
+                                        .fontWeight(.bold)
+                                        .foregroundStyle(.white)
+                                        .multilineTextAlignment(.leading)
+                                }
+                            }
+                            
+                        }
+                    }
                 }
                 Spacer()
             }
+        }
+        .task {
+            await viewModel.loadQuestions()
         }
     }
 }
 
 #Preview{
-    GameView()
+    //GameView()
+    let mockViewModel = GameViewModel()
+    mockViewModel.questions = [
+        Question(
+            type: "multiple",
+            difficulty: "easy",
+            category: "Entertainment: Film",
+            question: "What is the name of the protagonist in 'Cast Away'?",
+            correctAnswer: "Wilson",
+            incorrectAnswers: ["Carson", "Jackson", "Willy"]
+        ),
+        Question(
+            type: "multiple",
+            difficulty: "medium",
+            category: "Entertainment: Film",
+            question: "Which movie was NOT released in 1996?",
+            correctAnswer: "Gladiator",
+            incorrectAnswers: ["Independence Day", "The Rock", "Mission: Impossible"]
+        ),
+        Question(
+            type: "multiple",
+            difficulty: "hard",
+            category: "Entertainment: Film",
+            question: "Which animated movie first featured a celebrity voice actor?",
+            correctAnswer: "Aladdin",
+            incorrectAnswers: ["Toy Story", "James and the Giant Peach", "The Hunchback of Notre Dame"]
+        )
+    ]
+    mockViewModel.isLoading = false
+    
+    return GameView(viewModel: mockViewModel)
 }
--- TheMillionaireGame/GameModule/ViewModel/GameViewModel.swift
+++ TheMillionaireGame/GameModule/ViewModel/GameViewModel.swift
@@ -11,18 +11,33 @@ final class GameViewModel: ObservableObject {
     @Published var questions: [Question] = []
     @Published var isLoading: Bool = false
     @Published var errorMessage: String? = nil
-    
+    @Published var currentTextQuestion = ""
+    @Published var numberCurrentQuestion = -1
+    @Published var answers = ["", "", "", ""]
+    @Published var correctAnswer = ""
+    let ABCD = ["A: ", "B: ", "C: ", "D: "]
+
     func loadQuestions() async {
         await MainActor.run { self.isLoading = true }
         await MainActor.run { self.errorMessage = nil }
         
         do {
             let questions = try await NetworkService.shared.fetchQuestions()
-            await MainActor.run { self.questions = questions }
+            await MainActor.run {
+                self.questions = questions
+                self.nextQuestion()
+                self.isLoading = false
+            }
         } catch {
             await MainActor.run { self.errorMessage = error.localizedDescription }
         }
-        
-        await MainActor.run { isLoading = false }
+    }
+    
+    func nextQuestion() {
+        numberCurrentQuestion += 1
+        print("numberCurrentQuestion = \(numberCurrentQuestion)")
+        currentTextQuestion = questions[numberCurrentQuestion].question
+        correctAnswer = questions[numberCurrentQuestion].correctAnswer
+        answers = ([correctAnswer] + questions[numberCurrentQuestion].incorrectAnswers).shuffled()
     }
 }
