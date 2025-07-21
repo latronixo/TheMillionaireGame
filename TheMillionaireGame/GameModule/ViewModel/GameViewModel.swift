5abb3f38de004b529f04f5792545e5ed31d2a625
Valentin <pamerra@mail.ru>
2025-07-21 22:30:19 +0300

    В GameViewModel реализовал получение массива вопросов

--- TheMillionaireGame/GameModule/ViewModel/GameViewModel.swift
+++ TheMillionaireGame/GameModule/ViewModel/GameViewModel.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+final class GameViewModel: ObservableObject {
+    @Published var questions: [Question] = []
+    @Published var isLoading: Bool = false
+    @Published var errorMessage: String? = nil
+    
+    func loadQuestions() async {
+        await MainActor.run { self.isLoading = true }
+        await MainActor.run { self.errorMessage = nil }
+        
+        do {
+            let questions = try await NetworkService.shared.fetchQuestions()
+            await MainActor.run { self.questions = questions }
+        } catch {
+            await MainActor.run { self.errorMessage = error.localizedDescription }
+        }
+        
+        await MainActor.run { isLoading = false }
+    }
+}
