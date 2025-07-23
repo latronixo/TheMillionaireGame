5320f36255c347a0c656579cfb036321e5f453ac
Chingiz <Dzhabaev@outlook.com>
2025-07-23 22:47:57 +0300

    Исправил LogoView для возможности изменения размера на других экранах.

--- TheMillionaireGame/SplashView/LogoView.swift
+++ TheMillionaireGame/SplashView/LogoView.swift
@@ -8,11 +8,13 @@
 import SwiftUI
 
 struct LogoView: View {
+    var size: CGSize = CGSize(width: 267, height: 267)
+    
     var body: some View {
         GeometryReader { geometry in
             Image("logo")
                 .resizable()
-                .frame(width: 267, height: 267)
+                .frame(width: size.width, height: size.height)
                 .shadow(color: Color.black.opacity(1.0), radius: 67)
                 .position(
                     x: geometry.size.width / 2,
